68d7ba373d1ff0b60d2af31ea911e085d2654e3d
Juraj Lapcak <lapcakjuraj@gmail.com>
2021-02-28 23:24:15 +0100

    uploading files by js post request with fetch json php api

--- upload.php
+++ upload.php
@@ -3,30 +3,56 @@
 <head>
     <title>File Uploader</title>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main-style.css">
-    <link rel="stylesheet" href="css/panels.css">
+    <script src="js/uploadScript.js"></script>
 </head>
 <body>
-<div class="page">
-    <header class="site-header">
-        <div class="branding-panel center-panel">
+<div class="container">
+    <header class="row site-header">
+        <div class="col-lg">
             <h1 id="main-branding">File Uploader</h1>
         </div>
     </header>
-    <main class="site-content">
-        <div class="center-panel">
-            <form id="file-uploader" method="post" action="uploader.php" enctype="multipart/form-data">
-                <label for="filename">Filename</label>
-                <input name="filename" type="text" id="filename" placeholder="File name">
-                <label for="filename">File</label>
-                <input name="fileinput" type="file" id="fileinput">
-                <input type="submit" id="submit">
-            </form>
+    <main class="row site-content">
+        <div class="col-lg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg">
+                        <a class="link-text" href="index.php">
+                            Back to table
+                            <a>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg">
+                            <form id="file-uploader" method="post" enctype="multipart/form-data">
+                                <div class="mt-3 mb-3">
+                                    <label for="file-name">Filename</label>
+                                    <input type="text" name="fileName" id="file-name" placeholder="File name">
+                                </div>
+                                <div class="mt-3 mb-3">
+                                    <label for="file-input">File</label>
+                                    <input type="file" name="fileInput" id="file-input">
+                                </div>
+                                <div class="mt-3 mb-3">
+                                    <button type="button" id="submit" class="btn btn-outline-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
-    <footer class="site-footer">
-        Toto je koniec stranky.
+    <footer class="row site-footer">
+        <div class="col-lg">
+            <p>
+                Toto je koniec stranky.
+            </p>
+        </div>
     </footer>
 </div>
+
 </body>
 </html>
