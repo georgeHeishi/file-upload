c8d90245e7a9a0d4c035baa0fccbcf708d26056f
Juraj Lapcak <lapcakjuraj@gmail.com>
2021-03-01 10:51:42 +0100

    sorting table refactor

--- js/sortTable.js
+++ js/sortTable.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
             } else {
                 /*If no switching has been done AND the direction is "asc",
                 set the direction to "desc" and run the while loop again.*/
-                if (switchcount === 0 && dir ==="asc") {
+                if (switchcount === 0 && dir === "asc") {
                     dir = "desc";
                     switching = true;
                 }
